c24ded56f7d525b008233e7af4d11f3fa66b9d88
thanhhuhu <nguyendinhthanh2002@gmail.com>
2024-10-18 18:56:10 +0700

    Vector

---
+++ Vector.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+vector<int> v;
+v.push_back(100);
+v.push_back(200);
+cout <<v.size()<<endl;
+for ( int i = 0; i < v.size(); i++)
+{
+	cout <<v[i]<<" ";
+}
+cout <<endl;
+// su dung range base for loop de duyet mang va in ra mang
+//for ( int x : v) cout <<x<< " "; 
+
+ 
+ 
+ return 0;
+}
+
+
